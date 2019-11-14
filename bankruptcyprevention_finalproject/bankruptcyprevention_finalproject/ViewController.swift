04cc809de96dddd249fc777653997f21c23c0b14
Lawrence <e0176571@u.nus.edu>
2019-11-14 16:29:52 -0600

    Initial Commit

---
+++ bankruptcyprevention_finalproject/bankruptcyprevention_finalproject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bankruptcyprevention_finalproject
+//
+//  Created by Lawrence on 14/11/19.
+//  Copyright © 2019 Lawrence Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
